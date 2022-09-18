5680cc197e50d83d0fb49ff9b4d98a209d2a4c99
hlh <614307727@qq.com>
2022-09-18 19:13:23 +0800

    first

---
+++ src/main/java/com/linhao/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.linhao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping
+public class PageController {
+    @GetMapping("/{url}")
+    public String url(@PathVariable("url") String url){
+        return url;
+    }
+
+}
